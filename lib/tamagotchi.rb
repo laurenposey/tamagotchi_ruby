c2b1aaf645553af589c3a81d3834a69269c245ba
Alexander Jacks and Lauren Posey <alexander@>
2016-07-06 15:35:44 -0700

    adds pet attributes on creation of pet, and tests reducing the attributes

--- lib/tamagotchi.rb
+++ lib/tamagotchi.rb
@@ -1,26 +1,63 @@
 class Tamagotchi
-  @@tamagotchi = {@name => "Kitty", @food => 10, @sleep => 10, @activity => 10}
+  @@my_pet = {@name => "Kitty", @food => 10, @sleepy => 10, @activity => 10}
 
   define_method(:initialize) do |name|
     @name = name
     @food = 10
-    @sleep = 10
+    @sleepy = 10
     @activity = 10
   end
 
+  define_method(:name) do
+    @name
+  end
+  define_method(:food) do
+    @food
+  end
+  define_method(:sleepy) do
+    @sleepy
+  end
+  define_method(:activity) do
+    @activity
+  end
+
+  define_method(:time_stamp) do
+    @@time_stamp
+  end
+
   define_method(:is_alive?) do
-    if !(@food==0 && @sleep==0 && @activity==0)
-    #   is_alive? = true
-    # else
-    #   is_alive? = false
-    # end
+    if !(@food==0 || @sleepy==0 || @activity==0)
+      is_alive = true
+    else
+      is_alive = false
+    end
   end
 
-  define_singleton_method(:food) do
-    @@tamagotchi << 'food unit (1 feeding press)'
+  define_method(:set_food) do
+    until (@food == 0)
+      @food -= 1
+    end
+  end
+  define_method(:set_sleepy) do
+    until (@sleepy == 0)
+      @sleepy -= 1
+    end
+  end
+  define_method(:set_activity) do
+    until (@activity == 0)
+      @activity -= 1
+    end
   end
 
-  define_singleton_method(:play) do
-    @@tamagotchi << 'play unit (1 feeding press)'
+  define_method(:interval) do
+    time = Time.new(2016,07,06,15,0,0, "+09:00")
+    time_passed = Time.now()
+    interval = (time_passed - time).to_i()
   end
+  define_method(:time_passed) do |interval|
+    @food -= (interval).to_i()
+    @sleepy -= (interval).to_i()
+    @activity -= (interval).to_i()
+  end
+
 end
--- spec/tamagotchi_spec.rb
+++ spec/tamagotchi_spec.rb
@@ -1,35 +1,47 @@
 require("rspec")
-require("places_been")
+require("tamagotchi")
 
 describe(Tamagotchi) do
   describe("#initialize") do
     it("sets the name and life levels of a new Tamagotchi") do
-      my_pet = Tamagotchi.new("lil dragon")
-      expect(my_pet.name()).to(eq("lil dragon"))
-      expect(my_pet.food_level()).to(eq(10))
-      expect(my_pet.sleep_level()).to(eq(10))
-      expect(my_pet.activity_level()).to(eq(10))
+      my_pet = Tamagotchi.new("Kitty")
+      expect(my_pet.name()).to(eq("Kitty"))
+      expect(my_pet.food()).to(eq(10))
+      expect(my_pet.sleepy()).to(eq(10))
+      expect(my_pet.activity()).to(eq(10))
     end
   end
 
   describe("#is_alive?") do
     it("is alive if the food level is above 0") do
-      my_pet = Tamagotchi.new("lil dragon")
+      my_pet = Tamagotchi.new("Kitty")
       expect(my_pet.is_alive?()).to(eq(true))
     end
 
     it("is dead if the food level is 0") do
-      my_pet = Tamagotchi.new("lil dragon")
-      my_pet.set_food_level() #make a method that will change the food level of your tamagotchi.
+      my_pet = Tamagotchi.new("Kitty")
+      my_pet.set_food() #make a method that will change the food level of your tamagotchi.
+      expect(my_pet.is_alive?()).to(eq(false))
+    end
+    it("is dead if the sleepy level is 0") do
+      my_pet = Tamagotchi.new("Kitty")
+      my_pet.set_sleepy()
+      expect(my_pet.is_alive?()).to(eq(false))
+    end
+    it("is dead if the activity level is 0") do
+      my_pet = Tamagotchi.new("Kitty")
+      my_pet.set_activity()
       expect(my_pet.is_alive?()).to(eq(false))
     end
   end
 
-  describe("#time_passes") do
+
+
+  describe("#time_passed") do
     it("decreases the amount of food the Tamagotchi has left by 1") do
-      my_pet = Tamagotchi.new("lil dragon")
-      my_pet.time_passes()  #decide what trigger you will use to make time pass
-      expect(my_pet.food_level()).to(eq(9))
+      my_pet = Tamagotchi.new("Kitty")
+      my_pet.time_passed(1)
+      expect(my_pet.food()).to(eq(9))
     end
   end
 end
